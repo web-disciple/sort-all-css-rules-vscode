1d506bbd87b3413eb2ab83a9f84b53ceea84aa21
Matton <45633887+monsieurwai@users.noreply.github.com>
2020-10-09 16:02:10 +0200

    v0

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,97 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+// // The module 'vscode' contains the VS Code extensibility API
+// // Import the module and reference it with the alias vscode in your code below
+// import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+// // this method is called when your extension is activated
+// // your extension is activated the very first time the command is executed
+// export function activate(context: vscode.ExtensionContext) {
+//   // Use the console to output diagnostic information (console.log) and errors (console.error)
+//   // This line of code will only be executed once when your extension is activated
+//   console.log(
+//     'Congratulations, your extension "sort-all-css-rules-in-css-files" is now active!'
+//   );
+
+//   // The command has been defined in the package.json file
+//   // Now provide the implementation of the command with registerCommand
+//   // The commandId parameter must match the command field in package.json
+//   let disposable = vscode.commands.registerCommand(
+//     "sort-all-css-rules-in-css-files.sorting",
+//     () => {
+//       const editor: any = vscode.window.activeTextEditor;
+
+//       if (editor) {
+//         let filePath = editor._documentData._document.fileName;
+//         let fileExtension: string = filePath
+//           .split(".")
+//           [filePath.split(".").length - 1].toLowerCase();
+//         console.log(fileExtension);
+//         if (fileExtension === "css") {
+//           let document = editor.document;
+// 		  let documentText: string = document.getText();
+// 		  editor.edit(editBuilder => {
+// 			  console.log(editBuilder);
+// 		  })
+//           documentText = documentText.replace(/(\r\n|\n|\r)/gm, "");
+//           documentText = documentText.replace(/\s/g, "");
+//           let documentFormated: any = documentText.split("{");
+//           //   let documentFormated: any = documentText.split(new RegExp("(?<=\{)(.*?)(?=\})"));
+//           console.log(documentFormated);
+//           vscode.window.showInformationMessage(
+//             "All css in the files has been sorted !"
+//           );
+//         } else {
+//           vscode.window.showInformationMessage("You need css file.");
+//         }
+//       }
+//     }
+//   );
+
+//   context.subscriptions.push(disposable);
+// }
+
+// // this method is called when your extension is deactivated
+// export function deactivate() {}
+
+import * as vscode from "vscode";
+
 export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand(
+    "sort-all-css-rules-in-css-files.sorting",
+    function () {
+      // Get the active text editor
+      const editor = vscode.window.activeTextEditor;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "sort-all-css-rules-in-css-files" is now active!');
+      if (editor) {
+        const document = editor.document;
+        const selection = editor.selection;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('sort-all-css-rules-in-css-files.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+        // Get the css within the selection
+        let css = document.getText(selection);
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Sort all css rules in css files!');
-	});
+        // Format property css
+        let reordered: Array<any> = [];
+        let formated: any = css;
+        formated = formated.replace(/(\r\n|\n|\r)/gm, "");
+        formated = formated.replace(/\s/g, "");
+        formated = formated.split(new RegExp("(?<={)(.*?)(?=})"));
+        formated.forEach((element: any, index: number) => {
+          let reorderedElement: any = element;
+          if ((index + 1) % 2 === 0) {
+            reorderedElement = reorderedElement.split(";");
+            reorderedElement = reorderedElement.sort();
+            reorderedElement = reorderedElement.join(";");
+            reorderedElement = reorderedElement.substring(1);
+          }
+          reordered.push(reorderedElement);
+        });
+        const finalReordered = reordered.join("")
+        console.log(finalReordered);
+        editor.edit(editBuilder => {
+        	editBuilder.replace(selection, finalReordered);
+        });
+      }
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
-
-// this method is called when your extension is deactivated
-export function deactivate() {}
